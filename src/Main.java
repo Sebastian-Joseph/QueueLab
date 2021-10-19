47ad4e34e73a91fcc80b0e259cce865d65a91c6f
Sebastian-Joseph <theclimax25@gmail.com>
2021-10-19 09:21:04 -0400

    added booths for tickets

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.*;
 
 public class Main {
+    public static void main(String[] args) {
+        LinkedList<String> booths = new LinkedList<String>();
+        booths.add("1");
+        booths.add("2");
+        booths.add("3");
+        booths.add("4");
+        System.out.println(booths);
+
+        Iterator iterator = booths.iterator();
+
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next()+" ");
+        }
+    }
 }
